9b3d34156b358380994adabd13c2374af40a8b9b
Igor Boloban <igor.boloban@netcracker.com>
2026-04-28 19:01:21 +0300

    feat: improve code analysis, robustness and concurrency control

--- code_rag/core/manager.py
+++ code_rag/core/manager.py
@@ -1,6 +1,6 @@
 import logging
 import asyncio
-from typing import List
+from typing import List, Optional
 from .interfaces import IStorage, IParser, IIntelligence
 from .models import KnowledgeUnit
 
